976810a6d17283912313e8d8728dbafb5ef2f237
harshkaria <harsh.karia1@gmail.com>
2020-05-07 01:41:57 -0700

    refactor frontend and fix bug

--- frontend/index.js
+++ frontend/index.js
@@ -1,31 +1,43 @@
 // Async function for API call
 // grabs the links from the database
-// TODO: add current day as parameter?
 
-let globalLinks = []
+// TODO: add current day as parameter?
+'use strict';
+
+// Returns a promise when complete
 async function grabData()
 {
+    // Calls the api and populates `response` when done
     let response = await fetch(`http://localhost:3001/api/links`)
+    // Waits for response to be populated and then converts it to JSON
     let data = await response.json();
     return data;
 }
-grabData().then((links, rej) => {
-    if(rej) 
-        return rej;
-    populateDom(links);
-    })
-
+//Populates the table
 function populateDom(links) {
+    // Grab the table from the html file
     var table = document.getElementById("linksTable")
+    // For each link we create a new row in the table
     links.forEach(link => {
         var row = table.insertRow()
+        // Create the columns
         var urlData = row.insertCell(0)
         var captionData = row.insertCell(1)
+        // Populate the url and cqption rows
         var urlText = document.createTextNode(link["url"])
         var captionText = document.createTextNode(link["caption"])
+        // Add the text to the respective columns
         urlData.appendChild(urlText)
         captionData.appendChild(captionText)
-
-
     })
 }
+
+    // Grabs the data and then populates the table
+    grabData().then((links, rej) => {
+    if(rej) 
+        return rej;
+    //console.log(links)
+    return links
+    }).then((links) => {
+        populateDom(links)
+    })
