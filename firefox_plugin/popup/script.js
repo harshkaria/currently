f384700153706afe823b5b697d932b26e7ae9fc6
harshkaria <harsh.karia1@gmail.com>
2020-05-13 02:44:19 -0700

    fix update race condition

--- firefox_plugin/popup/script.js
+++ firefox_plugin/popup/script.js
@@ -2,7 +2,7 @@ var doc = window.top.document
 var url = ''
 var title = ''
 const API_URL =  `http://localhost:3001/api/links`
-const debug = true;
+const debug = false;
 if(debug != true) 
     API_URL = `https://currentlydeployment.herokuapp.com/api/links`
 function populateTabInfo(title, url) {
